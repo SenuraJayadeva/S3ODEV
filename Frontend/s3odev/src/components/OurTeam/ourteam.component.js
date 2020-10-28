9b739377025bf4465da9cd1038a41d6279a37488
SenuraJayadeva <senurajayadeva@gmail.com>
2020-10-28 22:16:46 +0530

    updated social icon

--- Frontend/s3odev/src/components/OurTeam/ourteam.component.js
+++ Frontend/s3odev/src/components/OurTeam/ourteam.component.js
@@ -5,6 +5,8 @@ import Oshen from "./img/oshen.jpg";
 import Sandaru from "./img/sandaru.jpg";
 import Sameera from "./img/sameera.jpg";
 
+import "./ourteam.css";
+
 export default function Ourteam() {
   return (
     <div className="container" style={{ marginTop: "5%" }}>
@@ -12,63 +14,71 @@ export default function Ourteam() {
       <div className="row">
         <div className="col-md-3">
           <center>
-            <div className="card">
+            <div className="card socialicon">
               <img
                 src={Senura}
                 style={{ width: "200px", borderRadius: "50%" }}
               />
-
               <h4>
                 Senura Jayadeva <br />
                 Full Stack Developer
               </h4>
+              <i class="fab fa-facebook-square socialicons"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <i class="fab fa-linkedin socialicons"></i>
             </div>
           </center>
         </div>
 
         <div className="col-md-3">
           <center>
-            <div className="card">
+            <div className="card socialicon">
               <img
                 src={Oshen}
                 style={{ width: "200px", borderRadius: "50%" }}
               />
-
               <h4>
                 Oshen Dikkumbura <br />
                 Full Stack Developer
               </h4>
+              <i class="fab fa-facebook-square socialicons"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <i class="fab fa-linkedin socialicons"></i>
             </div>
           </center>
         </div>
 
         <div className="col-md-3">
           <center>
-            <div className="card">
+            <div className="card socialicon">
               <img
                 src={Sandaru}
                 style={{ width: "200px", borderRadius: "50%" }}
               />
-
               <h4>
                 Sandaru Shashinda <br />
                 Frontend Developer
               </h4>
+              <i class="fab fa-facebook-square socialicons"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <i class="fab fa-linkedin socialicons"></i>
             </div>
           </center>
         </div>
         <div className="col-md-3">
           <center>
-            <div className="card">
+            <div className="card socialicon">
               <img
                 src={Sameera}
                 style={{ width: "200px", borderRadius: "50%" }}
               />
-
               <h4>
                 Sameera Arachchige <br />
                 UI / UX Engineer
               </h4>
+              <i class="fab fa-facebook-square socialicons"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <i class="fab fa-linkedin socialicons"></i>
             </div>
           </center>
         </div>
